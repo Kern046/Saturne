3597806d0bc589bd3f50151c81dc8d17a77a2660
Axel Venet <kern046@gmail.com>
2015-01-20 22:21:47 +0100

    Initialize EventManager in the EngineKernel

--- src/Saturne/Core/Kernel/EngineKernel.php
+++ src/Saturne/Core/Kernel/EngineKernel.php
@@ -2,9 +2,42 @@
 
 namespace Saturne\Core\Kernel;
 
+use Saturne\Manager\EventManager;
+
+/**
+ * @name EngineKernel
+ * @authro Axel Venet <axel-venet@developtech.fr>
+ */
 class EngineKernel implements KernelInterface
 {
+    private $eventManager;
+    
     public function __construct()
     {
+        $this->setEventManager();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function setEventManager()
+    {
+        $this->eventManager = new EventManager();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getEventManager()
+    {
+        return $this->eventManager;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function throwEvent($event)
+    {
+        $this->eventManager->transmit($event);
     }
 }
--- src/Saturne/Core/Kernel/KernelInterface.php
+++ src/Saturne/Core/Kernel/KernelInterface.php
@@ -2,7 +2,28 @@
 
 namespace Saturne\Core\Kernel;
 
+/**
+ * @name KernelInterface
+ * @author Axel Venet <axel-venet@developtech.fr>
+ */
 interface KernelInterface
 {
+    /**
+     * Initialize the EventManager
+     */
+    public function setEventManager();
     
+    /**
+     * Get the EventManager
+     * 
+     * @return EventManager
+     */
+    public function getEventManager();
+    
+    /**
+     * Send an event to the EventManager for a broadcast diffusion to the listeners.
+     * 
+     * @param string $event
+     */
+    public function throwEvent($event);
 }
