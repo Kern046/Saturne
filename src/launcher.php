ce07e3f981f2fd030502d4ff63fe4799209ea8a2
Axel Venet <kern046@gmail.com>
2015-01-23 16:41:48 +0100

    Add possibility to pass parameters to a script and specify a target for the launcher

--- src/launcher.php
+++ src/launcher.php
@@ -1,11 +1,38 @@
 <?php
 
+// The first index is the script name
+array_shift($argv);
+
+function getScriptOptions($args)
+{
+    $options = [];
+    foreach($args as $arg)
+    {
+        $data = explode('=', $arg);
+        $options[substr($data[0], 2)] = $data[1];
+    }
+    return $options;
+}
+
+function getTarget($options)
+{
+    return
+        (isset($options['target']))
+        ? $options['target'] . '.php'
+        : 'engine.php'
+    ;
+}
+
+$options = getScriptOptions($argv);
+$target = getTarget($options);
+$implodedOptions = implode(' ', $argv);
+
 $config = [
     'WINNT' => [
-        'command' => 'start php "' . __DIR__ . DIRECTORY_SEPARATOR . 'engine.php"'
+        'command' => 'start php "' . __DIR__ . DIRECTORY_SEPARATOR . $target . '" ' . $implodedOptions
     ],
     'Linux' => [
-        'command' => 'php "' . __DIR__ . DIRECTORY_SEPARATOR . 'engine.php" &'
+        'command' => 'php "' . __DIR__ . DIRECTORY_SEPARATOR . $target . '" ' . $implodedOptions . ' &'
     ]
 ];
 
