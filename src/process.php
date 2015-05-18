5a4ab4c6dfa65793bfc974be301075a2e4dc9d38
Axel Venet <avenet@kilix.fr>
2015-05-18 18:23:47 +0200

    Delete old code

--- src/process.php
+++ src/process.php
@@ -5,13 +5,3 @@ require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR 
 $process = new Saturne\Core\Kernel\ProcessKernel();
 $process->init();
 $process->run($options);
-/**
-$name = $options['process'];
-fputs(STDOUT, json_encode([
-    'emmitter' => $name,
-    'memory' => memory_get_usage(),
-    'allocated-memory' => memory_get_usage(true)
-]));
-sleep(rand(2,10));
- *
- */
