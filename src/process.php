d5e3606fcbdb81ff4f20eb440f297835f10c8f38
Axel Venet <avenet@kilix.fr>
2015-05-18 18:22:43 +0200

    Call the new Kernel when initializing a process

--- src/process.php
+++ src/process.php
@@ -1,5 +1,11 @@
 <?php
 
+require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+$process = new Saturne\Core\Kernel\ProcessKernel();
+$process->init();
+$process->run($options);
+/**
 $name = $options['process'];
 fputs(STDOUT, json_encode([
     'emmitter' => $name,
@@ -7,3 +13,5 @@ fputs(STDOUT, json_encode([
     'allocated-memory' => memory_get_usage(true)
 ]));
 sleep(rand(2,10));
+ *
+ */
