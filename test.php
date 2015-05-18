ae4fc8f92f2437b3eb5a1e3abf6c93fd55508c96
Axel Venet <avenet@kilix.fr>
2015-05-18 18:23:22 +0200

    Update test file

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 $socket = stream_socket_client('tcp://127.0.0.1:8889');
 
 stream_socket_sendto($socket, json_encode([
-    'action' => 'client:connect',
+    'action' => 'server:shutdown',
     'user-agent' => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
 ]));
 
